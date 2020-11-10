06b0f96cfc65dbbd55c67f83e878798ee11d04e8
dianahub250 <niyongogadidi@gmail.com>
2020-11-09 22:33:34 -1200

    adding update

--- lib/Tasks.dart
+++ lib/Tasks.dart
@@ -4,7 +4,7 @@ import 'package:Tasks/View-tasks.Dart';
 
 void main() {
   runApp(MaterialApp(
-    title: 'Todo ',
+    title: 'Todo',
     theme: ThemeData(primaryColor: Colors.red),
     home: Tasks(),
   ));
@@ -16,6 +16,11 @@ class Tasks extends StatefulWidget {
 }
 
 class _TasksState extends State<Tasks> {
+  int selectedIndex;
+  String title;
+  String text = "No Value Entered";
+  String title2;
+  String text2 = "No Value Entered";
   TextEditingController etTaskname = new TextEditingController();
   TextEditingController etTasktime = new TextEditingController();
 
@@ -68,116 +73,187 @@ class _TasksState extends State<Tasks> {
         });
   }
 
+  void _setText() {
+    setState(() {
+      if (selectedIndex == null) {
+        bool isValid = validate();
+        if (!isValid) {
+          return null;
+        }
+        myarray.add(Mytask(
+          etTaskname.text,
+          etTasktime.text,
+        ));
+        alterMessage("Task Added Successful");
+      } else {
+        myarray.elementAt(selectedIndex).name = etTaskname.text;
+        myarray.elementAt(selectedIndex).time = etTasktime.text;
+
+        selectedIndex = null;
+      }
+      etTaskname.clear();
+      etTasktime.clear();
+    });
+  }
+
+  // List to hold all tasks
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text('To Do Tasks'),
         backgroundColor: Colors.red,
-        title: Text('Todo Tasks'),
       ),
-      body: Form(
+      body: SingleChildScrollView(
         child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(top: 35.0),
-            ),
+          children: [
             Text(
-              'Welcome to our system',
+              'Welcome To Our System',
               style: TextStyle(
                   fontWeight: FontWeight.bold,
-                  fontSize: 26.0,
+                  fontSize: 18.0,
                   color: Colors.red),
             ),
             SizedBox(
-              height: 12.0,
+              height: 8.0,
             ),
             Text(
-              'Create Your To Do List Here',
+              'Create Your Task here',
               style: TextStyle(
                 fontWeight: FontWeight.bold,
                 fontSize: 18.0,
               ),
             ),
+            SizedBox(
+              height: 8.0,
+            ),
+
             Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: TextFormField(
-                controller: etTaskname,
-                decoration: InputDecoration(
-                    // contentPadding:  const EdgeInsets.only(left: 3.0),
-                    hintText: 'Please Enter Task Name',
-                    errorText:
-                        taskNameError == "" ? "" : "Please enter task name"),
-              ),
+              padding: const EdgeInsets.all(15),
+              child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Task',
+                    border: OutlineInputBorder(),
+                  ),
+                  controller: etTaskname),
+            ),
+            SizedBox(
+              height: 8,
             ),
             Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: TextFormField(
-                controller: etTasktime,
+              padding: const EdgeInsets.all(15),
+              child: TextField(
                 decoration: InputDecoration(
-                  hintText: 'Task Time',
-                  errorText:
-                      taskTimeError == "" ? "" : "Please enter task time",
+                  labelText: 'Time',
+                  border: OutlineInputBorder(),
                   suffixIcon: IconButton(
-                      icon: Icon(Icons.timer),
-                      onPressed: () async {
-                        TimeOfDay picked = await showTimePicker(
-                            context: context,
-                            initialTime: TimeOfDay.now(),
-                            builder: (BuildContext context, Widget child) {
-                              return MediaQuery(
-                                data: MediaQuery.of(context)
-                                    .copyWith(alwaysUse24HourFormat: true),
-                                child: child,
-                              );
-                            });
-                        setState(() {
-                          etTasktime.text = picked.format(context);
-                        });
-                      }),
+                    icon: Icon(Icons.timer),
+                    onPressed: () async {
+                      TimeOfDay picked = await showTimePicker(
+                        context: context,
+                        initialTime: TimeOfDay.now(),
+                        builder: (BuildContext context, Widget child) {
+                          return MediaQuery(
+                            data: MediaQuery.of(context)
+                                .copyWith(alwaysUse24HourFormat: true),
+                            child: child,
+                          );
+                        },
+                      );
+                      setState(() {
+                        etTasktime.text = picked.format(context);
+                      });
+                    },
+                  ),
                 ),
+                controller: etTasktime,
               ),
             ),
-            Container(
-              child: Row(
-                children: [
-                  SizedBox(width: 80.0),
-                  MaterialButton(
-                    onPressed: () {
-                      bool isValid = validate();
-                      if (!isValid) {
-                        return null;
-                      }
-                      myarray.add(Mytask(etTaskname.text, etTasktime.text));
-                      etTaskname.clear();
-                      etTasktime.clear();
-                      alterMessage("Task Added Successful");
-                      task.name = etTaskname.text;
-                      task.time = etTasktime.text;
-                    },
-                    color: Colors.red,
-                    textColor: Colors.white,
-                    child: Text('Add'),
-                  ),
-                  SizedBox(width: 12.0),
-                  MaterialButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        CupertinoPageRoute(
-                          builder: (BuildContext context) => ViewScreen(
-                            mytask: myarray,
-                          ),
+            Row(
+              children: [
+                SizedBox(
+                  height: 8,
+                ),
+                SizedBox(
+                  width: 80,
+                ),
+                RaisedButton(
+                  onPressed: _setText,
+                  child: Text(selectedIndex == null ? 'Add' : 'Save'),
+                  color: Colors.red,
+                  textColor: Colors.white,
+                  elevation: 8,
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                MaterialButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      CupertinoPageRoute(
+                        builder: (BuildContext context) => ViewScreen(
+                          mytask: myarray,
                         ),
-                      );
-                    },
-                    color: Colors.red,
-                    textColor: Colors.white,
-                    child: Text('View'),
-                  ),
-                ],
+                      ),
+                    );
+                  },
+                  color: Colors.red,
+                  textColor: Colors.white,
+                  child: Text('View'),
+                ),
+              ],
+            ),
+
+            // List to display tasks
+            SizedBox(
+<<<<<<< HEAD
+              height: 200,
+              child: ListView.builder(
+                itemCount: myarray.length,
+                itemBuilder: (ctx, index) {
+                  return ListTile(
+                    title: Text(myarray.elementAt(index).name),
+                    subtitle: Text(myarray.elementAt(index).time),
+                    trailing: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          selectedIndex = index;
+                        });
+                        etTaskname.text = myarray.elementAt(index).name;
+                        etTasktime.text = myarray.elementAt(index).time;
+                      },
+                      icon: Icon(Icons.edit),
+                    ),
+                  );
+                },
               ),
-            )
+            ),
+=======
+                height: 200,
+                child: ListView.builder(
+                  itemCount: tasks.length,
+                  itemBuilder: (ctx, index) {
+                    return ListTile(
+                      title: Text(tasks.elementAt(index).title),
+                      subtitle: Text(tasks.elementAt(index).time),
+                      trailing: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            selectedIndex = index;
+                          });
+                          smsCodeController.text = tasks.elementAt(index).title;
+                          smsCodeController2.text = tasks.elementAt(index).time;
+                        },
+                        icon: Icon(Icons.edit),
+                      ),
+                    );
+                  },
+                )),
+            // changes in text
+            // are shown here
+>>>>>>> ac5e6d0 (updating)
           ],
         ),
       ),
